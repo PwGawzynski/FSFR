57e2344d9cd06a141276e8bddc77894448c2cbec
pwgawzynski <pgawzynski.backup@gmail.com>
2023-02-25 19:05:17 +0100

    Reset pwd screen done

--- Components/Pages/ResetPassword.tsx
+++ Components/Pages/ResetPassword.tsx
@@ -1,5 +1,63 @@
-import { View } from 'react-native';
+import {
+  LayoutAnimation,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+  UIManager,
+  View,
+} from 'react-native';
+import React, { useState } from 'react';
+import { LogoImageCurtain } from '../Atoms/LogoImageCurtain';
+import { InfoText } from '../Atoms/InfoText';
+import { AppButton } from '../Atoms/AppButton';
+import { AppInput } from '../Molecules/AppInput';
+import { ResetPasswordData } from '../../../farm-service-be/types/Useer/LoginDataObject';
 
 export function ResetPassword() {
-  return <View className="w-screen h-screen" />;
+  const [data, setData] = useState({ email: '' } as ResetPasswordData);
+  const [isInputFocused, setInputFocused] = useState(false as boolean);
+  const inputRef = React.createRef<TextInput>();
+  return (
+    <TouchableOpacity
+      activeOpacity={1}
+      className="w-screen h-screen bg-white items-center"
+      onPress={() => inputRef.current?.blur()}
+    >
+      <LogoImageCurtain />
+      <InfoText additionalStyles="w-9/12 mt-24">
+        ENTER YOUR ACCOUNT E-MAIL ADDRESS OR NIP OR USER NAME AND WE WILL SEND
+        YOU MAIL WITH LINK TO RESET YOUR PASSWORD
+      </InfoText>
+      <AppInput
+        refGetter={inputRef}
+        onFocus={() => {
+          if (
+            Platform.OS === 'android' &&
+            UIManager.setLayoutAnimationEnabledExperimental
+          ) {
+            UIManager.setLayoutAnimationEnabledExperimental(true);
+          }
+          LayoutAnimation.easeInEaseOut();
+          setInputFocused(true);
+        }}
+        onDeFocus={() => setInputFocused(false)}
+        additionalStyles="w-9/12 mt-10"
+        setter={setData}
+        ObjectKey="email"
+        value={data.email}
+        underlyingLabel="UserName or NIP or E-mail"
+      />
+      <View
+        className={`w-full  items-center flex-1 ${
+          isInputFocused ? 'justify-start' : 'justify-end'
+        }`}
+      >
+        <AppButton
+          additionalStyles="w-9/12 mb-28 mt-20"
+          action={() => console.log('Reset password')}
+          context="Reset your password"
+        />
+      </View>
+    </TouchableOpacity>
+  );
 }
