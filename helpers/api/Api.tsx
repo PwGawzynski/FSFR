b611c786000210268c99909f472ac3d2b9835b9d
pwgawzynski <pgawzynski.backup@gmail.com>
2023-03-26 20:16:57 +0200

    added data to send, rem error handling from fn and move to react query system

--- helpers/api/Api.tsx
+++ helpers/api/Api.tsx
@@ -8,39 +8,32 @@ export class Api {
     timeout: 5000,
     withCredentials: true,
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTG9naW4iOiJ1c2VyMjIiLCJ1c2VySWQiOiJiMWU4MmM0Yy1iMzA0LTRlNzMtYTU0ZS05MmY5MTU4MTA1ZWYiLCJpYXQiOjE2Nzk1OTc1MzksImV4cCI6MTY3OTU5ODQzOX0.8CboaDrGzg0LooecL9M9Q0T1jpWpVFjVqjK-V64DvQk`,
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTG9naW4iOiJ1c2VyMjIiLCJ1c2VySWQiOiJiMWU4MmM0Yy1iMzA0LTRlNzMtYTU0ZS05MmY5MTU4MTA1ZWYiLCJpYXQiOjE2Nzk2MDE5ODEsImV4cCI6MTY3OTYwMjg4MX0.8z-XS5wOQO0ExsYpfqP4YaOlmMUaoLIWio-Fw7FMebo`,
     },
   });
 
   static async registerNewUser(userData: RegisterScreensDataCollection) {
-    try {
-      const serializedData = {
-        email: userData.email,
-        userPersonalData: {
-          name: userData.name,
-          surname: userData.surname,
-          phoneNumber: userData.contactPhone,
-        },
-        addressData: {
-          city: userData.city,
-          county: userData.county,
-          street: userData.street,
-          apartmentNumber: userData.apartmentNumber,
-          voivodeship: userData.voivodeship,
-          houseNumber: userData.houseNumber,
-          postalCode: userData.postalCode,
-        },
-        accountData: {
-          theme: 0,
-        },
-        userRole: userData.userRole,
-      } as RegisterNewUserDataDtoInterfaceMobi;
-      return Api.axiosInstance
-        .post('/user', serializedData)
-        .then(res => console.log(res.data))
-        .catch(e => e);
-    } catch (e) {
-      return '';
-    }
+    const serializedData = {
+      email: userData.email,
+      userPersonalData: {
+        name: userData.name,
+        surname: userData.surname,
+        phoneNumber: userData.contactPhone,
+      },
+      addressData: {
+        city: userData.city,
+        county: userData.county,
+        street: userData.street,
+        apartmentNumber: userData.apartmentNumber,
+        voivodeship: userData.voivodeship,
+        houseNumber: userData.houseNumber,
+        postalCode: userData.postalCode,
+      },
+      accountData: {
+        theme: 0,
+      },
+      userRole: userData.userRole,
+    } as RegisterNewUserDataDtoInterfaceMobi;
+    return Api.axiosInstance.post('/user', serializedData);
   }
 }
