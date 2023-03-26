0d57421e79fc1a5fd673f73f2437e912b77be2fa
pwgawzynski <pgawzynski.backup@gmail.com>
2023-03-26 20:45:51 +0200

    created fn that checks if session didn't expire

--- helpers/api/Api.tsx
+++ helpers/api/Api.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
 import { RegisterScreensDataCollection } from '../../FrontendSelfTypes/RegisterMobi/RegisterScreensData';
 import { RegisterNewUserDataDtoInterfaceMobi } from '../../FarmServiceTypes/User/RegisterNewUserDataDtoInterfaceMobi';
+import { RestoreTokenStoredObject } from '../../FrontendSelfTypes/IToken/RestoreTokenStoredObject';
 
 export class Api {
   private static axiosInstance = axios.create({
@@ -11,6 +13,14 @@ export class Api {
       Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTG9naW4iOiJ1c2VyMjIiLCJ1c2VySWQiOiJiMWU4MmM0Yy1iMzA0LTRlNzMtYTU0ZS05MmY5MTU4MTA1ZWYiLCJpYXQiOjE2Nzk2MDE5ODEsImV4cCI6MTY3OTYwMjg4MX0.8z-XS5wOQO0ExsYpfqP4YaOlmMUaoLIWio-Fw7FMebo`,
     },
   });
+
+  static async checkCurrentSession() {
+    const session = await SecureStore.getItemAsync('RefreshToken');
+    if (!session) return false;
+    const token: RestoreTokenStoredObject = JSON.parse(session);
+    const now = new Date();
+    return now.getTime() - token.lastUpdatedAt.getTime() > 604800000;
+  }
 
   static async registerNewUser(userData: RegisterScreensDataCollection) {
     const serializedData = {
