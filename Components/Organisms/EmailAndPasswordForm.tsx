86a7ee2fb728406f039ae3f6a12adcf7c38278d2
pwgawzynski <pgawzynski.backup@gmail.com>
2023-03-27 23:01:53 +0200

    added mutation to serve identity handle

--- Components/Organisms/EmailAndPasswordForm.tsx
+++ Components/Organisms/EmailAndPasswordForm.tsx
@@ -1,14 +1,30 @@
 import { TextInput, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
+import { useMutation } from 'react-query';
+import { AxiosError } from 'axios';
 import { AppInput } from '../Molecules/AppInput';
 import { AppButton } from '../Atoms/AppButton';
 import { OrLabel } from '../Atoms/OrLabel';
-import { EmailAndPasswordData } from '../../../FarmServiceBE/farm-service-be/types/Useer/RegisterDataObject';
 import { RegisterMobiPropsBase } from '../../frontendSelfTypes/navigation/types';
 import {
   handleRestoreData,
   handleSaveDataMerge,
 } from '../../helpers/handlers/AsyncStoreHelpers';
+import { EmailAndPasswordData } from '../../FrontendSelfTypes/RegisterMobi/RegisterScreensData';
+import { Api } from '../../helpers/api/Api';
+import {
+  ResponseCode,
+  ResponseObject,
+} from '../../FarmServiceTypes/Respnse/responseGeneric';
+import { ErrorInfoText } from '../Atoms/ErrorInfoText';
+
+function handleErrorOccurred(e: unknown) {
+  if (!(e instanceof AxiosError))
+    return 'Some error occurred, please try again later';
+  const message = e.response?.data?.payload?.message;
+  if (!message) return 'Some error occurred, please try again later';
+  return message;
+}
 
 export function EmailAndPasswordForm({
   navigation,
@@ -19,13 +35,23 @@ export function EmailAndPasswordForm({
     email: '',
     password: '',
   });
+
+  const [canMoveOn, setCanMoveOn] = useState(false);
+
+  const registerToIdentity = useMutation(
+    async (mutationData: EmailAndPasswordData) => {
+      const response = (await Api.registerInAuthUser(mutationData))
+        .data as ResponseObject;
+      if (response.code === ResponseCode.ProcessedCorrect) setCanMoveOn(true);
+    },
+  );
   useEffect(() => {
     (async () => {
       await handleRestoreData('RegisterMobiDataEmailAndPassword', setData);
     })();
   }, []);
   return (
-    <View className="w-10/12 pt-10">
+    <View className="w-10/12 pt-10 items-center">
       <AppInput
         autoFocus
         onSubmit={() => input2.current?.focus()}
@@ -56,14 +82,16 @@ export function EmailAndPasswordForm({
         }
       />
       <AppButton
-        action={() =>
-          handleSaveDataMerge(
-            'RegisterMobiDataEmailAndPassword',
-            data,
-            navigation,
-            'ChooseUserRole',
-          )
-        }
+        action={() => {
+          registerToIdentity.mutate(data);
+          if (registerToIdentity.isSuccess)
+            handleSaveDataMerge(
+              'RegisterMobiDataEmailAndPassword',
+              data,
+              navigation,
+              'ChooseUserRole',
+            );
+        }}
         context="Next"
         additionalStyles="mt-10 mb-2"
       />
@@ -73,6 +101,11 @@ export function EmailAndPasswordForm({
         action={() => console.log('Register w Google')}
         context="Use Google account To register"
       />
+      {registerToIdentity.isError && (
+        <ErrorInfoText additionalStyles="mt-10">
+          {handleErrorOccurred(registerToIdentity.error)}
+        </ErrorInfoText>
+      )}
     </View>
   );
 }
