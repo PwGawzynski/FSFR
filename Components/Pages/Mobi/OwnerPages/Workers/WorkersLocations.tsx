c4a8b671bedb751beb5e88c3da7628e364482316
pwgawzynski <pgawzynski.backup@gmail.com>
2023-08-22 15:53:40 +0200

    create page component base

---
+++ Components/Pages/Mobi/OwnerPages/Workers/WorkersLocations.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from 'react-native';
+
+export function WorkersLocations() {
+  return (
+    <SafeAreaView>
+      <Text>Workers locations</Text>
+    </SafeAreaView>
+  );
+}
