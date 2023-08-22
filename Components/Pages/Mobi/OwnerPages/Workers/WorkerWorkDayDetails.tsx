ebe2bcab87cd848e15952f08abbd3288dea1b247
pwgawzynski <pgawzynski.backup@gmail.com>
2023-08-22 15:54:00 +0200

    create page component base

---
+++ Components/Pages/Mobi/OwnerPages/Workers/WorkerWorkDayDetails.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from 'react-native';
+
+export function WorkerWorkDayDetails() {
+  return (
+    <SafeAreaView>
+      <Text>Worker workday details</Text>
+    </SafeAreaView>
+  );
+}
