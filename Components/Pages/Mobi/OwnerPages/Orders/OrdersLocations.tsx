fb7f60f6a4d448ae6f4381d943c79cf6ebc2b7ce
pwgawzynski <pgawzynski.backup@gmail.com>
2023-08-22 15:07:40 +0200

    create page component base

---
+++ Components/Pages/Mobi/OwnerPages/Orders/OrdersLocations.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from 'react-native';
+
+export function OrdersLocations() {
+  return (
+    <SafeAreaView>
+      <Text>ORDERS locations</Text>
+    </SafeAreaView>
+  );
+}
