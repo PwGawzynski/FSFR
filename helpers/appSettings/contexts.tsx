37dbf79755db6fc1e679a006d839f731c9bf3f1e
pwgawzynski <pgawzynski.backup@gmail.com>
2023-08-22 14:57:48 +0200

    added user data to context

--- helpers/appSettings/contexts.tsx
+++ helpers/appSettings/contexts.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { DeviceType } from 'expo-device';
+import { GetUserDataResponse } from '../../FarmServiceTypes/Respnse/UserService/GetUserDataResponse';
 // _______________________APP_SETTINGS_______________________ //
 export enum ThemeOptions {
   dark,
@@ -15,6 +16,9 @@ export interface ModalContext {
   isOn: ModalState;
   context?: string;
 
+  customApproveButtonText?: string;
+  customDisapproveButtonText?: string;
+
   onApproveCallback?: () => void;
   onDisapproveCallback?: () => void;
 }
@@ -26,9 +30,14 @@ interface SettingsObject {
   isLogged: boolean;
 
   modalContext: ModalContext;
+
+  currentUser: GetUserDataResponse | undefined;
 }
 interface AppOptionsSetters {
   setTheme: React.Dispatch<React.SetStateAction<ThemeOptions>>;
+  setCurrentUser: React.Dispatch<
+    React.SetStateAction<GetUserDataResponse | undefined>
+  >;
   setDeviceType: React.Dispatch<React.SetStateAction<DeviceType>>;
   setLogged: React.Dispatch<React.SetStateAction<boolean>>;
   setModalContext: React.Dispatch<React.SetStateAction<ModalContext>>;
