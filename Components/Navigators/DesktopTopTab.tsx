e5c705e9f1075b955be553ea7b22a809f2822f86
pwgawzynski <pgawzynski.backup@gmail.com>
2023-08-22 15:00:35 +0200

    created desktop top tab navigator

---
+++ Components/Navigators/DesktopTopTab.tsx
@@ -0,0 +1,24 @@
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { topLabelOff } from '../../helpers/style/TopLabelOff';
+import { DesktopTopTabParamList } from '../../FrontendSelfTypes/NavigatorsInterfaces/DesktopTopTabParamList';
+import { DesktopRootMobi } from '../Pages/DesktopRootMobi';
+import { DesktopNotificationCenter } from '../Pages/DesktopNotificationCenter';
+
+const TabNavigator = createMaterialTopTabNavigator<DesktopTopTabParamList>();
+
+export function DesktopTopTab() {
+  return (
+    <TabNavigator.Navigator>
+      <TabNavigator.Screen
+        options={topLabelOff}
+        name="desktopRoot"
+        component={DesktopRootMobi}
+      />
+      <TabNavigator.Screen
+        options={topLabelOff}
+        name="notifications"
+        component={DesktopNotificationCenter}
+      />
+    </TabNavigator.Navigator>
+  );
+}
