065d5bf0dbb83481bf52f1c62a585ccbbd122bcd
pwgawzynski <pgawzynski.backup@gmail.com>
2023-08-23 11:31:31 +0200

    created bell icon component

---
+++ Components/Atoms/BellIco.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+import BellSvg from '../../assets/bell.svg';
+
+export function BellIco() {
+  return (
+    <View className="items-end">
+      <BellSvg fill="#000" width={40} height={40} />
+      <View className="w-4 h-4 relative top-[-16] right-8 bg-red-600 rounded-full">
+        <Text className="text-white text-center">1</Text>
+      </View>
+    </View>
+  );
+}
