830f28f3efaa8c836c763a3642a327906b3c50ca
pwgawzynski <pgawzynski.backup@gmail.com>
2023-08-24 10:45:02 +0200

    refactored, split to different components

--- Components/Pages/DesktopRootMobi.tsx
+++ Components/Pages/DesktopRootMobi.tsx
@@ -3,16 +3,19 @@ import React from 'react';
 import { DesktopRootHeader } from '../Molecules/DesktopRootHeader';
 import { DesktopBottomButtons } from '../Molecules/DesktopBottomButtons';
 import { OwnerMobiDesktopTopTabProps } from '../../FrontendSelfTypes/navigation/types';
+import { Api } from '../../helpers/api/Api';
+import { LastActivities } from '../Organisms/LastActivities';
 
 export function DesktopRootMobi({
   navigation,
   route,
 }: OwnerMobiDesktopTopTabProps<'desktopRoot', 'desktop'>) {
+  Api.getAllActivities();
   return (
-    <SafeAreaView>
+    <SafeAreaView className="mr-4 ml-4  h-full">
       <DesktopRootHeader />
+      <LastActivities />
       <DesktopBottomButtons navigation={navigation} route={route} />
-      <View className="flex flex-col" />
     </SafeAreaView>
   );
 }
