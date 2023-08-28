3ce5e0e8e8b760c36b4b80d40e923ce6a0162329
pwgawzynski <pgawzynski.backup@gmail.com>
2023-08-28 10:10:18 +0200

    Added typing to Activity Service

--- helpers/api/Services/Activities.tsx
+++ helpers/api/Services/Activities.tsx
@@ -1,9 +1,12 @@
 import { Api } from '../Api';
+import { ActivityI } from '../../../FrontendSelfTypes/moduleProps/ComponentsProps';
 
-export async function getLastActivitiesService() {
+export async function getLastActivitiesService(): Promise<
+  Array<ActivityI> | undefined
+> {
   try {
     const data = await Api.getAllActivities();
-    return data.lastActivities;
+    return data.lastActivities as Array<ActivityI>;
   } catch (e) {
     return undefined;
   }
