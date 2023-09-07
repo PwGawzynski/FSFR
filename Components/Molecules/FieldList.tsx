5594324ded03f082444580bdc7e68820f24f6940
pwgawzynski <pgawzynski.backup@gmail.com>
2023-09-07 19:33:02 +0200

    Created field list witch fetch all fields assigned to order

---
+++ Components/Molecules/FieldList.tsx
@@ -0,0 +1,96 @@
+import { ScrollView, Text, View } from 'react-native';
+import { DataTable } from 'react-native-paper';
+import { useQuery } from 'react-query';
+import {
+  FieldI,
+  FieldListProps,
+  FieldStatus,
+} from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
+import { getAllFieldsByOrderId } from '../../helpers/api/Services/FieldsService';
+
+export function FieldList({ orderId }: FieldListProps) {
+  const { data: orderConnectedFields } = useQuery<Array<FieldI> | undefined>(
+    ['field', orderId],
+    ({ queryKey }) => getAllFieldsByOrderId(`${queryKey[1]}`),
+  );
+  return (
+    <View className="grow">
+      <DataTable className="flex-1 w-full h-full">
+        <DataTable.Header>
+          <DataTable.Title>
+            <Text className="text-black text-sm uppercase font-bold">LP</Text>
+          </DataTable.Title>
+          <DataTable.Title>
+            <Text className="text-black text-sm uppercase font-bold">Area</Text>
+          </DataTable.Title>
+          <DataTable.Title>
+            <Text className="text-black text-sm uppercase font-bold">
+              FieldId
+            </Text>
+          </DataTable.Title>
+          <DataTable.Title
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'flex-end',
+            }}
+          >
+            <Text className="text-black text-sm uppercase font-bold">
+              Status
+            </Text>
+          </DataTable.Title>
+        </DataTable.Header>
+        <ScrollView
+          className="w-max h-max"
+          showsVerticalScrollIndicator={false}
+        >
+          {orderConnectedFields &&
+            orderConnectedFields.map((f: FieldI, i) => (
+              <DataTable.Row key={f.fieldId}>
+                <DataTable.Cell
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'flex-start',
+                  }}
+                  numeric
+                >
+                  <Text className="text-black text-sm">{`${i}`}</Text>
+                </DataTable.Cell>
+                <DataTable.Cell
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'flex-start',
+                  }}
+                  numeric
+                >
+                  <Text className="text-black text-sm">{f.area}</Text>
+                </DataTable.Cell>
+                <DataTable.Cell
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'flex-start',
+                  }}
+                >
+                  <Text className="text-black text-sm">{f.fieldId}</Text>
+                </DataTable.Cell>
+                <DataTable.Cell
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'flex-end',
+                  }}
+                >
+                  <Text className="text-black text-sm">
+                    {FieldStatus[f.status]}
+                  </Text>
+                </DataTable.Cell>
+              </DataTable.Row>
+            ))}
+        </ScrollView>
+      </DataTable>
+    </View>
+  );
+}
