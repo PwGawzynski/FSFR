e7f1ad6df045632d509b8cd9d80a92e9eba57d7d
pwgawzynski <pgawzynski.backup@gmail.com>
2023-09-11 14:13:29 +0200

    set up navigator

--- Components/Navigators/FieldsTopTab.tsx
+++ Components/Navigators/FieldsTopTab.tsx
@@ -1,13 +1,18 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { FieldTopTabParamList } from '../../FrontendSelfTypes/NavigatorsInterfaces/FieldTopTabParamList';
 import { FieldDetails } from '../Pages/Mobi/OwnerPages/Fields/FieldDetails';
+import { topLabelOff } from '../../helpers/style/TopLabelOff';
 
 export function FieldsTopTab() {
   const TabNavigator = createMaterialTopTabNavigator<FieldTopTabParamList>();
 
   return (
     <TabNavigator.Navigator>
-      <TabNavigator.Screen name="fieldDetails" component={FieldDetails} />
+      <TabNavigator.Screen
+        options={topLabelOff}
+        name="fieldDetails"
+        component={FieldDetails}
+      />
     </TabNavigator.Navigator>
   );
 }
