34be07380c6247ccaa062d57838f336d3fb61dd5
pwgawzynski <pgawzynski.backup@gmail.com>
2023-09-18 23:26:37 +0200

    added props

--- Components/Atoms/ProfilePhoto.tsx
+++ Components/Atoms/ProfilePhoto.tsx
@@ -1,4 +1,5 @@
-import { Image, View } from 'react-native';
+import { Image, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
 import { ProfilePhotoProps } from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
 import UserSvg from '../../assets/user.svg';
 
@@ -7,10 +8,13 @@ export function ProfilePhoto({
   additionalImageStyles,
   additionalBoxStyles,
   imgLink,
+  focused,
 }: ProfilePhotoProps) {
   return (
     <View
-      className={`w-8 h-8 rounded-full overflow-hidden items-center justify-center ${additionalBoxStyles}`}
+      className={`w-8 h-8 rounded-full overflow-hidden items-center justify-center ${
+        focused && 'border-solid border-[#279840] border-4'
+      } ${additionalBoxStyles}`}
     >
       {imgLink ? (
         <Image
