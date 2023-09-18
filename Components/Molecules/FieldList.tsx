e248262ba014e2f58b2b42b5cbc669d4431e8ae5
pwgawzynski <pgawzynski.backup@gmail.com>
2023-09-18 23:23:00 +0200

    repair performance

--- Components/Molecules/FieldList.tsx
+++ Components/Molecules/FieldList.tsx
@@ -1,108 +1,60 @@
 import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
-import { DataTable } from 'react-native-paper';
+import { ActivityIndicator, DataTable } from 'react-native-paper';
 import { useQuery } from 'react-query';
+
+import { useMemo } from 'react';
 import {
   FieldI,
   FieldListProps,
-  FieldStatus,
 } from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
 import { getAllFieldsByOrderId } from '../../helpers/api/Services/FieldsService';
+import { FieldTableRow } from '../Organisms/FieldTableRow';
+import { FieldTableHeaders } from '../Organisms/FieldTableHeaders';
 
-export function FieldList({ orderId, navigation }: FieldListProps) {
+export function FieldList({
+  orderId,
+  navigation,
+  lPOff,
+  shownFieldKeys,
+  checkOn,
+  setSelected,
+}: FieldListProps) {
   const { data: orderConnectedFields } = useQuery<Array<FieldI> | undefined>(
     ['fields', orderId],
     ({ queryKey }) => getAllFieldsByOrderId(`${queryKey[1]}`),
   );
+  console.log(orderConnectedFields === undefined);
+  const fieldList = useMemo(() => {
+    return (
+      <FieldTableRow
+        fields={orderConnectedFields}
+        navigation={navigation}
+        lPOff
+        columnNames={shownFieldKeys}
+        checkOn
+        setSelected={setSelected}
+      />
+    );
+  }, [orderConnectedFields]);
   return (
     <View className="grow">
-      <DataTable className="flex-1 w-full h-full">
-        <DataTable.Header>
-          <DataTable.Title>
-            <Text className="text-black text-sm uppercase font-bold">LP</Text>
-          </DataTable.Title>
-          <DataTable.Title>
-            <Text className="text-black text-sm uppercase font-bold">Area</Text>
-          </DataTable.Title>
-          <DataTable.Title>
-            <Text className="text-black text-sm uppercase font-bold">
-              FieldId
-            </Text>
-          </DataTable.Title>
-          <DataTable.Title
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'flex-end',
-            }}
+      {orderId ? (
+        <DataTable className="flex-1 w-full h-full">
+          <ScrollView
+            className="w-max h-max"
+            showsVerticalScrollIndicator={false}
           >
-            <Text className="text-black text-sm uppercase font-bold">
-              Status
-            </Text>
-          </DataTable.Title>
-        </DataTable.Header>
-        <ScrollView
-          className="w-max h-max"
-          showsVerticalScrollIndicator={false}
-        >
-          {orderConnectedFields &&
-            orderConnectedFields.map((f: FieldI, i) => (
-              <TouchableOpacity
-                key={f.fieldId}
-                onPress={() =>
-                  navigation.navigate('fields', {
-                    screen: 'fieldDetails',
-                    params: {
-                      fieldId: f.fieldId,
-                    },
-                  })
-                }
-              >
-                <DataTable.Row>
-                  <DataTable.Cell
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'row',
-                      justifyContent: 'flex-start',
-                    }}
-                    numeric
-                  >
-                    <Text className="text-black text-sm">{`${i}`}</Text>
-                  </DataTable.Cell>
-                  <DataTable.Cell
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'row',
-                      justifyContent: 'flex-start',
-                    }}
-                    numeric
-                  >
-                    <Text className="text-black text-sm">{f.area}</Text>
-                  </DataTable.Cell>
-                  <DataTable.Cell
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'row',
-                      justifyContent: 'flex-start',
-                    }}
-                  >
-                    <Text className="text-black text-sm">{f.fieldId}</Text>
-                  </DataTable.Cell>
-                  <DataTable.Cell
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'row',
-                      justifyContent: 'flex-end',
-                    }}
-                  >
-                    <Text className="text-black text-sm">
-                      {FieldStatus[f.status]}
-                    </Text>
-                  </DataTable.Cell>
-                </DataTable.Row>
-              </TouchableOpacity>
-            ))}
-        </ScrollView>
-      </DataTable>
+            <FieldTableHeaders
+              checkOn={checkOn}
+              lPOff={lPOff}
+              columnNames={shownFieldKeys}
+            />
+            {orderConnectedFields && fieldList}
+          </ScrollView>
+        </DataTable>
+      ) : (
+        <ActivityIndicator animating />
+      )}
     </View>
   );
 }
