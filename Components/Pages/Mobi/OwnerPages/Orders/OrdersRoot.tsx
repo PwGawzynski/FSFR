ef7e9c2ea56169850c9f711c5027629098295a69
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-09 15:15:49 +0200

    added sorting, changed header and order list

--- Components/Pages/Mobi/OwnerPages/Orders/OrdersRoot.tsx
+++ Components/Pages/Mobi/OwnerPages/Orders/OrdersRoot.tsx
@@ -1,8 +1,9 @@
 import { SafeAreaView } from 'react-native';
 import React from 'react';
 import { OrdersRootScreenTopBar } from '../../../../Organisms/OrdersRootScreenTopBar';
-import { OrdersShortInfoBoxList } from '../../../../Organisms/OrdersShortInfoBoxList';
+import { OrdersList } from '../../../../Organisms/OrdersList';
 import { OwnerMobiOrdersTopTabProps } from '../../../../../FrontendSelfTypes/navigation/types';
+import { SmallHeader } from '../../../../Molecules/SmallHeader';
 
 export function OrdersRoot({
   route,
@@ -11,7 +12,17 @@ export function OrdersRoot({
   return (
     <SafeAreaView className="flex flex-col h-full w-ful  ml-4 mr-4 mt-4">
       <OrdersRootScreenTopBar navigation={navigation} route={route} />
-      <OrdersShortInfoBoxList navigation={navigation} route={route} />
+      <SmallHeader>Upcoming Orders</SmallHeader>
+      <OrdersList
+        navigation={navigation}
+        route={route}
+        sort={(a, b) =>
+          new Date(a.performanceDate).getTime() >=
+          new Date(b.performanceDate).getTime()
+            ? 1
+            : -1
+        }
+      />
     </SafeAreaView>
   );
 }
