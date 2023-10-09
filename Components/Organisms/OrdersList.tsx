c95d3fba50bf75ba218dad289c984d30c1cb69b9
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-09 23:23:17 +0200

    added new props, added reloadIndicator

--- Components/Organisms/OrdersList.tsx
+++ Components/Organisms/OrdersList.tsx
@@ -2,11 +2,24 @@ import { View } from 'react-native';
 import React from 'react';
 import { OrdersListProps } from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
 import Orders from './Orders';
+import { OrdersTopTabParamList } from '../../FrontendSelfTypes/NavigatorsInterfaces/OrdersTopTabParamList';
 
-export function OrdersList({ navigation, route, sort }: OrdersListProps) {
+export function OrdersList<T extends keyof OrdersTopTabParamList>({
+  navigation,
+  route,
+  sort,
+  filterMethod,
+  reloadIndicator,
+}: OrdersListProps<T>) {
   return (
     <View className="flex-1 flex flex-col">
-      <Orders navigation={navigation} route={route} sort={sort} />
+      <Orders
+        reloadIndicator={reloadIndicator}
+        navigation={navigation}
+        route={route}
+        sort={sort}
+        filterMethod={filterMethod}
+      />
     </View>
   );
 }
