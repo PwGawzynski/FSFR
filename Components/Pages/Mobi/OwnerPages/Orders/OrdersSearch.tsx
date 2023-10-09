79e0a5fe19db3b2148236db7551a6e41aea874bc
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-09 23:24:07 +0200

    created search screen

--- Components/Pages/Mobi/OwnerPages/Orders/OrdersSearch.tsx
+++ Components/Pages/Mobi/OwnerPages/Orders/OrdersSearch.tsx
@@ -1,9 +1,37 @@
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
+import { useState } from 'react';
+import { ScreenTitleHeader } from '../../../../Atoms/ScreenTitleHeader';
+import { SearchEngine } from '../../../../Organisms/SearchEngine';
+import { OwnerMobiOrdersTopTabProps } from '../../../../../FrontendSelfTypes/navigation/types';
+import { OrdersList } from '../../../../Organisms/OrdersList';
 
-export function OrdersSearch() {
+export function OrdersSearch({
+  route,
+  navigation,
+}: OwnerMobiOrdersTopTabProps<'ordersSearch', 'orders'>) {
+  const INITI_SEARCH_VALUE = '';
+  const [searchValue, setSearchValue] = useState(INITI_SEARCH_VALUE);
   return (
-    <SafeAreaView>
-      <Text>ORDERS search</Text>
+    <SafeAreaView className="flex-col h-full justify-center mr-4 ml-4 ">
+      <ScreenTitleHeader
+        variant="lg"
+        additionalStyles="w-full justify-start mb-4"
+      >
+        Search Order
+      </ScreenTitleHeader>
+      <SearchEngine onSearchPress={text => setSearchValue(text)} />
+      <View className="flex-1 mt-8">
+        <OrdersList
+          navigation={navigation}
+          route={route}
+          filterMethod={order =>
+            searchValue === INITI_SEARCH_VALUE
+              ? true
+              : order.client.includes(searchValue)
+          }
+          reloadIndicator={searchValue}
+        />
+      </View>
     </SafeAreaView>
   );
 }
