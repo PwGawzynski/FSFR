e680b2ddd9697c0e4a3bad40004040017b58abb6
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-10 16:05:14 +0200

    added filters

--- Components/Pages/Mobi/OwnerPages/Orders/OrdersSearch.tsx
+++ Components/Pages/Mobi/OwnerPages/Orders/OrdersSearch.tsx
@@ -6,13 +6,31 @@ import { OwnerMobiOrdersTopTabProps } from '../../../../../FrontendSelfTypes/nav
 import { OrdersList } from '../../../../Organisms/OrdersList';
 import { EmptyListInfo } from '../../../../Molecules/EmptyListInfo';
 import EmptyIco from '../../../../../assets/empty.svg';
+import { FiltersSetter } from '../../../../Molecules/FiltersSetter';
+import { OrderBaseI } from '../../../../../FrontendSelfTypes/moduleProps/ComponentsProps';
+
+const filterOrderByKey = (
+  order: OrderBaseI,
+  filter: keyof OrderBaseI,
+  searchValue: string,
+) => {
+  if (order[filter] && typeof order[filter] === 'string')
+    return (order[filter] as string)
+      ?.toLowerCase()
+      .includes(searchValue.toLowerCase());
+  if (order[filter] && typeof order[filter] === 'number')
+    return order[filter]?.toString() === searchValue;
+  return false;
+};
 
 export function OrdersSearch({
   route,
   navigation,
 }: OwnerMobiOrdersTopTabProps<'ordersSearch', 'orders'>) {
-  const INITI_SEARCH_VALUE = '';
-  const [searchValue, setSearchValue] = useState(INITI_SEARCH_VALUE);
+  const INIT_SEARCH_VALUE = '';
+  const INIT_FILTER_NAME: keyof OrderBaseI = 'name';
+  const [searchValue, setSearchValue] = useState(INIT_SEARCH_VALUE);
+  const [filter, setFilter] = useState<keyof OrderBaseI>(INIT_FILTER_NAME);
   return (
     <SafeAreaView className="flex-col h-full justify-center mr-4 ml-4 ">
       <ScreenTitleHeader
@@ -21,6 +39,14 @@ export function OrdersSearch({
       >
         Search Order
       </ScreenTitleHeader>
+      <FiltersSetter<OrderBaseI>
+        onFilterOnOff={filterName => setFilter(filterName)}
+        filterOn={filter}
+        optionsRows={[
+          ['client', 'name'],
+          ['status', 'area', 'performanceDate'],
+        ]}
+      />
       <SearchEngine onSearchPress={text => setSearchValue(text)} />
       <View className="flex-1 mt-8">
         <OrdersList
@@ -32,9 +58,9 @@ export function OrdersSearch({
           navigation={navigation}
           route={route}
           filterMethod={order =>
-            searchValue === INITI_SEARCH_VALUE
+            searchValue === INIT_SEARCH_VALUE
               ? true
-              : order.client.toLowerCase().includes(searchValue.toLowerCase())
+              : filterOrderByKey(order, filter, searchValue)
           }
           reloadIndicator={searchValue}
         />
