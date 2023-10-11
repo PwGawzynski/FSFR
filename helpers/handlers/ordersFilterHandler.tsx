2d44971e2d03790376f9ce83aa25d6d113263dd8
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-11 10:22:38 +0200

    created filter functions used with search module

---
+++ helpers/handlers/ordersFilterHandler.tsx
@@ -0,0 +1,43 @@
+import {
+  OrderBaseI,
+  OrderStatus,
+} from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
+
+export const defaultOrdersFilterMethod = (
+  order: OrderBaseI,
+  filter: keyof OrderBaseI,
+  searchValue: string,
+  initSearchValue = '',
+) => {
+  console.log('def fn sort invoked');
+  if (order[filter]) {
+    if (searchValue === initSearchValue) return true;
+    if (typeof order[filter] === 'string')
+      return (order[filter] as string)
+        ?.toLowerCase()
+        .includes(searchValue.toLowerCase());
+    if (typeof order[filter] === 'number')
+      return order[filter]?.toString() === searchValue;
+  }
+  return false;
+};
+
+export const doneOrdersFilterMethod = (
+  order: OrderBaseI,
+  filter: keyof OrderBaseI,
+  searchValue: string,
+  initSearchValue = '',
+) => {
+  if (order.status === OrderStatus.Done) {
+    return defaultOrdersFilterMethod(
+      order,
+      filter,
+      searchValue,
+      initSearchValue,
+    );
+  }
+  return false;
+};
+
+export const UpcomingOrdersFilter = (order: OrderBaseI) =>
+  order.status !== OrderStatus.Done;
