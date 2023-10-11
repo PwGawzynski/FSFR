6db06554fb29fd55265fa385d340e4910f8875f3
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-11 10:11:02 +0200

    due to optional children added default ico

--- Components/Molecules/EmptyListInfo.tsx
+++ Components/Molecules/EmptyListInfo.tsx
@@ -1,10 +1,15 @@
 import { Text, View } from 'react-native';
 import { EmptyListProps } from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
+import EmptyIco from '../../assets/empty.svg';
 
-export function EmptyListInfo({ text, children: emoji }: EmptyListProps) {
+export function EmptyListInfo({ text, children }: EmptyListProps) {
   return (
     <View className="flex-1 flex-col items-center justify-start">
-      <View className="items-center justify-center">{emoji}</View>
+      <View className="items-center justify-center">
+        {children ?? (
+          <EmptyIco width={100} style={{ width: 100, height: 100 }} />
+        )}
+      </View>
       <Text className="text-xl text-[#848484]">{text}</Text>
     </View>
   );
