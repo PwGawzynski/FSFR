adeaccafea87f8cdf50e968238d67b76919aaf06
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-12 12:12:19 +0200

    created option object for stack nav

---
+++ helpers/style/OrdersStackScreenOptions.tsx
@@ -0,0 +1,5 @@
+import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
+
+export const OrdersStackScreenOptions = {
+  headerShown: false,
+} as NativeStackNavigationOptions;
