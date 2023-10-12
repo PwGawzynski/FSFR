d34308905e71cde18f36632ec8409c633d90eece
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-12 12:05:24 +0200

    changed filter method

--- Components/Pages/Mobi/OwnerPages/Orders/OrdersRoot.tsx
+++ Components/Pages/Mobi/OwnerPages/Orders/OrdersRoot.tsx
@@ -2,14 +2,17 @@ import { SafeAreaView } from 'react-native';
 import React from 'react';
 import { OrdersRootScreenTopBar } from '../../../../Organisms/OrdersRootScreenTopBar';
 import { OrdersList } from '../../../../Organisms/OrdersList';
-import { OwnerMobiOrdersTopTabProps } from '../../../../../FrontendSelfTypes/navigation/types';
+import {
+  OwnerMobiOrdersTopTabProps,
+  OwnerOrdersMaterialRootProps,
+} from '../../../../../FrontendSelfTypes/navigation/types';
 import { SmallHeader } from '../../../../Molecules/SmallHeader';
-import { UpcomingOrdersFilter } from '../../../../../helpers/handlers/ordersFilterHandler';
+import { OnlyOpenOrdersFilter } from '../../../../../helpers/handlers/ordersFilterHandler';
 
 export function OrdersRoot({
   route,
   navigation,
-}: OwnerMobiOrdersTopTabProps<'ordersRoot', 'orders'>) {
+}: OwnerOrdersMaterialRootProps<'materialOrdersRoot', 'ordersRoot', 'orders'>) {
   return (
     <SafeAreaView className="flex flex-col h-full w-ful  ml-4 mr-4 mt-4">
       <OrdersRootScreenTopBar navigation={navigation} route={route} />
@@ -17,7 +20,7 @@ export function OrdersRoot({
       <OrdersList
         navigation={navigation}
         route={route}
-        filterMethod={UpcomingOrdersFilter}
+        filterMethod={OnlyOpenOrdersFilter}
         sort={(a, b) =>
           new Date(a.performanceDate).getTime() >=
           new Date(b.performanceDate).getTime()
