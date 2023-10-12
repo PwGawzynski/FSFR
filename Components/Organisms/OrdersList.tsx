44455cf5b004c77a603ca1a4e2db50fd4b956994
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-12 17:49:16 +0200

    fix navi

--- Components/Organisms/OrdersList.tsx
+++ Components/Organisms/OrdersList.tsx
@@ -2,29 +2,18 @@ import { View } from 'react-native';
 import React from 'react';
 import { OrdersListProps } from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
 import Orders from './Orders';
-import { OrdersTopTabParamList } from '../../FrontendSelfTypes/NavigatorsInterfaces/OrdersTopTabParamList';
-import { MaterialOrdersRootTopTabParamList } from '../../FrontendSelfTypes/NavigatorsInterfaces/MaterialOrdersRootTopTabParamLIst';
-import { OwnerDesktopRootStackParamList } from '../../FrontendSelfTypes/NavigatorsInterfaces/OwnerDesktopRootStackParamList';
 
-export function OrdersList<
-  T extends keyof MaterialOrdersRootTopTabParamList,
-  N extends keyof OrdersTopTabParamList,
-  M extends keyof OwnerDesktopRootStackParamList,
->({
-  navigation,
-  route,
+export function OrdersList({
   sort,
   filterMethod,
   reloadIndicator,
   ListEmptyComponent,
-}: OrdersListProps<T, N, M>) {
+}: OrdersListProps) {
   return (
     <View className="flex-1 flex flex-col">
       <Orders
         ListEmptyComponent={ListEmptyComponent}
         reloadIndicator={reloadIndicator}
-        navigation={navigation}
-        route={route}
         sort={sort}
         filterMethod={filterMethod}
       />
