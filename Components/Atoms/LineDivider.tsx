136c829c9d33c172632b71de630b60c15e071c68
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-15 19:53:09 +0200

    refactored additional style names

--- Components/Atoms/LineDivider.tsx
+++ Components/Atoms/LineDivider.tsx
@@ -1,6 +1,6 @@
 import { View } from 'react-native';
 import { LineDividerProps } from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
 
-export function LineDivider({ additionalStyles }: LineDividerProps) {
-  return <View className={`h-[1px]  bg-black mt-4 mb-4 ${additionalStyles}`} />;
+export function LineDivider({ abs }: LineDividerProps) {
+  return <View className={`h-[1px]  bg-black mt-4 mb-4 ${abs}`} />;
 }
