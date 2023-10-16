8dabf9964e0f3899b13dd0c3009282883f0105cc
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-16 08:57:52 +0200

    fix import

--- Components/Atoms/InfoText.tsx
+++ Components/Atoms/InfoText.tsx
@@ -1,7 +1,7 @@
 import { Text } from 'react-native';
 import { useContext } from 'react';
 import { AppSettings, ThemeOptions } from '../../helpers/appSettings/contexts';
-import { InfoTextProps } from '../../frontendSelfTypes/moduleProps/ComponentsProps';
+import { InfoTextProps } from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
 
 export function InfoText({ children, abs }: InfoTextProps) {
   const context = useContext(AppSettings);
