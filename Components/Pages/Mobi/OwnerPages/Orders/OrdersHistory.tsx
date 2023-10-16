494ceb6b126fe5e1525b3c672acf807169e468d4
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-16 12:56:24 +0200

    rem route end navigation

--- Components/Pages/Mobi/OwnerPages/Orders/OrdersHistory.tsx
+++ Components/Pages/Mobi/OwnerPages/Orders/OrdersHistory.tsx
@@ -1,21 +1,13 @@
 import { SafeAreaView } from 'react-native';
 import { ScreenTitleHeader } from '../../../../Atoms/ScreenTitleHeader';
-import { OwnerMobiOrdersTopTabProps } from '../../../../../FrontendSelfTypes/navigation/types';
 import { OrdersListSearchAndFilter } from '../../../../Organisms/OrdersListSearchAndFilter';
 import { doneOrdersFilterMethod } from '../../../../../helpers/handlers/ordersFilterHandler';
 
-export function OrdersHistory({
-  route,
-  navigation,
-}: OwnerMobiOrdersTopTabProps<'ordersHistory', 'orders'>) {
+export function OrdersHistory() {
   return (
     <SafeAreaView className="flex-col h-full justify-center mr-4 ml-4 ">
       <ScreenTitleHeader variant="lg">Closed Orders</ScreenTitleHeader>
-      <OrdersListSearchAndFilter
-        navigation={navigation}
-        route={route}
-        filterMethod={doneOrdersFilterMethod}
-      />
+      <OrdersListSearchAndFilter filterMethod={doneOrdersFilterMethod} />
     </SafeAreaView>
   );
 }
