f942ba6bb3ccfee483e33e0d6dbf278cca5247c6
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-16 08:57:04 +0200

    fix eslint error

--- Components/Molecules/DesktopBottomButtons.tsx
+++ Components/Molecules/DesktopBottomButtons.tsx
@@ -1,16 +1,16 @@
 import { View } from 'react-native';
 import React from 'react';
 import { AppButton } from '../Atoms/AppButton';
-import { OwnerMobiDesktopTopTabProps } from '../../FrontendSelfTypes/navigation/types';
+import { OwnerMobiDesktopRootStackProps } from '../../FrontendSelfTypes/navigation/types';
 
 export function DesktopBottomButtons({
   navigation,
-}: OwnerMobiDesktopTopTabProps<'desktopRoot', 'desktop'>) {
+}: OwnerMobiDesktopRootStackProps<'desktop'>) {
   return (
     <View className="flex flex-row mb-10 mt-6">
       <AppButton
         abs="flex-1 mr-2"
-        action={() => navigation.navigate('orders', { screen: 'ordersRoot' })}
+        action={() => navigation.navigate('orders')}
         context="Orders"
       />
       <AppButton
