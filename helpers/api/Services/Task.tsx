70e61d41c44c10e8cfc6da62749c3c61a2f6d845
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-17 21:16:16 +0200

    added filtration to service method

--- helpers/api/Services/Task.tsx
+++ helpers/api/Services/Task.tsx
@@ -1,4 +1,7 @@
-import { AddNewTasksI } from '../../../FrontendSelfTypes/moduleProps/ComponentsProps';
+import {
+  AddNewTasksI,
+  OrderTask,
+} from '../../../FrontendSelfTypes/moduleProps/ComponentsProps';
 import { Api } from '../Api';
 
 export async function addNewTask(
@@ -6,6 +9,26 @@ export async function addNewTask(
 ): Promise<boolean | undefined> {
   try {
     return Api.addNewTasks(data);
+  } catch (e) {
+    return undefined;
+  }
+}
+
+export async function getAllOrdersTasks(
+  data: string,
+): Promise<Array<OrderTask> | undefined> {
+  try {
+    const res = await Api.getAllOrdersTasks(data);
+    const filtered: Array<OrderTask> = [];
+    res.forEach(
+      orderTask =>
+        !filtered.find(
+          filteredOrderTask =>
+            filteredOrderTask.worker.id === orderTask.worker.id,
+        ) && filtered.push(orderTask),
+    );
+    console.log(filtered, 'filtered');
+    return filtered;
   } catch (e) {
     return undefined;
   }
