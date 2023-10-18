ccfc1273b58b53903af004de52525c2e18762890
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-18 21:56:53 +0200

    created orderAssignedWorkers screen

--- Components/Pages/Mobi/OwnerPages/Orders/OrdersManageWorkers.tsx
+++ Components/Pages/Mobi/OwnerPages/Orders/OrdersManageWorkers.tsx
@@ -1,16 +1,31 @@
 import { SafeAreaView, View } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import { HeaderWithButton } from '../../../../Atoms/HeaderWithButton';
 import { OwnerMobiOrdersTopTabProps } from '../../../../../FrontendSelfTypes/navigation/types';
-import { Worker } from '../../../../../FrontendSelfTypes/moduleProps/ComponentsProps';
+import {
+  OrderTask,
+  Worker,
+} from '../../../../../FrontendSelfTypes/moduleProps/ComponentsProps';
 import { WorkerSelector } from '../../../../Organisms/WorkerSelector';
 import { getAllOrdersTasks } from '../../../../../helpers/api/Services/Task';
+import { WorkersTasksList } from '../../../../Organisms/WorkersTasksList';
+import { ScreenTitleHeader } from '../../../../Atoms/ScreenTitleHeader';
+
+const getExplicitWorkersEntities = (orderAssignedTasks: Array<OrderTask>) =>
+  orderAssignedTasks
+    .filter(
+      orderTask =>
+        orderAssignedTasks?.filter(
+          task => task.worker.id === orderTask.worker.id,
+        ).length === 1,
+    )
+    .map(orderTask => orderTask.worker);
 
 export function OrdersManageWorkers({
   navigation,
   route,
-}: OwnerMobiOrdersTopTabProps<'assignedWorkers', 'orders'>) {
+}: OwnerMobiOrdersTopTabProps<'ordersManageWorkers', 'orders'>) {
   const orderId = route.params?.orderId;
   const [focusedWorker, setFocusedWorker] = useState<Worker | undefined>(
     undefined,
@@ -19,35 +34,49 @@ export function OrdersManageWorkers({
     ['orderAssignedTasks', orderId],
     ({ queryKey }) => getAllOrdersTasks(`${queryKey[1]}`),
   );
+  const [selectedWorkerTasks, setSelectedWorkerTasks] = useState<
+    Array<OrderTask> | undefined
+  >(undefined);
+
+  useEffect(() => {
+    if (focusedWorker && orderAssignedTasks)
+      setSelectedWorkerTasks(
+        orderAssignedTasks.filter(
+          orderTask => orderTask.worker.id === focusedWorker.id,
+        ),
+      );
+  }, [focusedWorker]);
   return (
-    <SafeAreaView className="flex grow mr-4 ml-4">
-      <HeaderWithButton
-        variant="lg"
-        buttonAdditionalStyles="ml-4 flex-1"
-        headerText="Assigned Workers"
-        headerAdditionalStyles="flex-1"
-        boxAdditionalStyles="mt-8"
-        buttonText="Manage workers"
-        onButtonClick={() =>
-          navigation.navigate('assignedWorkers', { orderId })
-        }
-      />
-      <View className="mt-4">
-        <WorkerSelector
-          externalData
-          data={orderAssignedTasks
-            ?.filter(
-              orderTask =>
-                orderAssignedTasks?.filter(
-                  task => task.worker.id === orderTask.worker.id,
-                ).length === 1,
-            )
-            ?.map(orderTask => orderTask.worker)}
-          focusedWorker={focusedWorker}
-          setFocusedWorker={setFocusedWorker}
+    <SafeAreaView className="w-full h-full  bg-white">
+      <View className="flex  ml-2 mr-2">
+        <HeaderWithButton
+          variant="lg"
+          buttonAdditionalStyles="ml-4 flex-1"
+          headerText="Assigned Workers"
+          headerAdditionalStyles="flex-1"
+          boxAdditionalStyles="mt-8"
+          buttonText="Manage workers"
+          onButtonClick={() =>
+            navigation.navigate('assignedWorkers', { orderId })
+          }
         />
+        <View className="mt-4">
+          <WorkerSelector
+            externalData
+            data={
+              orderAssignedTasks &&
+              getExplicitWorkersEntities(orderAssignedTasks)
+            }
+            focusedWorker={focusedWorker}
+            setFocusedWorker={setFocusedWorker}
+          />
+        </View>
+        <ScreenTitleHeader variant="sm" abs="mt-6 mb-4">
+          {focusedWorker?.name} Task&apos;s
+        </ScreenTitleHeader>
+        {selectedWorkerTasks && <WorkersTasksList data={selectedWorkerTasks} />}
+        <View className="grow" />
       </View>
-      <View className="grow" />
     </SafeAreaView>
   );
 }
