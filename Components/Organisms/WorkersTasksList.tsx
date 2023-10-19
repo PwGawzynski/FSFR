3a63b1ab0b87dfd2acfe964f3331155af122a846
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-19 22:10:54 +0200

    created tasks list

--- Components/Organisms/WorkersTasksList.tsx
+++ Components/Organisms/WorkersTasksList.tsx
@@ -1,15 +1,39 @@
 import { FlatList } from 'react-native';
+import { useMutation } from 'react-query';
+import { useEffect, useMemo, useState } from 'react';
 import { WorkersTaskList } from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
 import { WorkersTaskListElement } from '../Molecules/WorkersTaskListElement';
+import { remTask } from '../../helpers/api/Services/Task';
 
 export function WorkersTasksList({ data }: WorkersTaskList) {
-  return (
-    <FlatList
-      className=""
-      data={data}
-      renderItem={({ item, index }) =>
-        WorkersTaskListElement({ ...item, index })
-      }
-    />
+  const { mutate: removeTask } = useMutation(remTask);
+  const [remTaskId, setRemTaskId] = useState<string | undefined>();
+  const afterRemData = data.filter(orderTask => {
+    console.log(orderTask.id !== remTaskId);
+    return orderTask.id !== remTaskId;
+  });
+  useEffect(() => {
+    if (remTaskId) removeTask(remTaskId);
+  }, [remTaskId]);
+  return useMemo(
+    () => (
+      <FlatList
+        data={afterRemData}
+        keyExtractor={({ id }) => id}
+        renderItem={({ item, index }) => (
+          <WorkersTaskListElement
+            worker={item.worker}
+            id={item.id}
+            index={index}
+            type={item.type}
+            onRemoveTask={taskId => {
+              setRemTaskId(taskId);
+            }}
+            field={item.field}
+          />
+        )}
+      />
+    ),
+    [afterRemData],
   );
 }
