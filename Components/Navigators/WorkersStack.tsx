ff45e25b6b70b1901ce0b4da4680a583085e71cf
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-23 14:59:34 +0200

    moved to new navigator, change navigator type

--- Components/Navigators/WorkersStack.tsx
+++ Components/Navigators/WorkersStack.tsx
@@ -1,15 +1,13 @@
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { WorkersTopTabParamList } from '../../FrontendSelfTypes/NavigatorsInterfaces/WorkersTopTabParamList';
-import { WorkersRoot } from '../Pages/Mobi/OwnerPages/Workers/WorkersRoot';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { WorkersStackParamList } from '../../FrontendSelfTypes/NavigatorsInterfaces/WorkersStackParamList';
 import { OrdersStackScreenOptions } from '../../helpers/style/OrdersStackScreenOptions';
 import { WorkerDetails } from '../Pages/Mobi/OwnerPages/Workers/WorkerDetails';
-import { WorkerSearch } from '../Pages/Mobi/OwnerPages/Workers/WorkerSearch';
 import { WorkersLocations } from '../Pages/Mobi/OwnerPages/Workers/WorkersLocations';
 import { WorkerWorkDayDetails } from '../Pages/Mobi/OwnerPages/Workers/WorkerWorkDayDetails';
 import { WorkerWorkDaysInfo } from '../Pages/Mobi/OwnerPages/Workers/WorkerWorkDaysInfo';
-import { AddWorker } from '../Pages/Mobi/OwnerPages/Workers/AddWorker';
+import { MaterialWorkersTopTab } from './MaterialWorkersTopTab';
 
-const TabNavigator = createMaterialTopTabNavigator<WorkersTopTabParamList>();
+const TabNavigator = createNativeStackNavigator<WorkersStackParamList>();
 
 export function WorkersTabNavigator() {
   return (
@@ -17,22 +15,12 @@ export function WorkersTabNavigator() {
       <TabNavigator.Screen
         options={OrdersStackScreenOptions}
         name="workersRoot"
-        component={WorkersRoot}
-      />
-      <TabNavigator.Screen
-        options={OrdersStackScreenOptions}
-        name="addWorker"
-        component={AddWorker}
+        component={MaterialWorkersTopTab}
       />
       <TabNavigator.Screen
         options={OrdersStackScreenOptions}
         name="workerDetails"
         component={WorkerDetails}
-      />
-      <TabNavigator.Screen
-        options={OrdersStackScreenOptions}
-        name="workerSearch"
-        component={WorkerSearch}
       />
       <TabNavigator.Screen
         options={OrdersStackScreenOptions}
