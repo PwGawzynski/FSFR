ebcffad969e4377b99093d144357daf1a8769748
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-23 14:59:54 +0200

    moved params to new navigator

---
+++ FrontendSelfTypes/NavigatorsInterfaces/WorkersStackParamList.tsx
@@ -0,0 +1,13 @@
+import { MaterialWorkersRootTopTabParamList } from './MaterialWorkersRootTopTabParamList';
+
+export type WorkersStackParamList = {
+  workersRoot: MaterialWorkersRootTopTabParamList;
+
+  workersLocation: undefined;
+
+  workerDetails: undefined;
+
+  workerWorkDayDetails: undefined;
+
+  workerWorkDaysInfo: undefined;
+};
