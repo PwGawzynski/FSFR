27309cec79304b8017ccdc73c8d0f6baf4212da0
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-24 12:41:04 +0200

    created worker root screen

--- Components/Pages/Mobi/OwnerPages/Workers/WorkersRoot.tsx
+++ Components/Pages/Mobi/OwnerPages/Workers/WorkersRoot.tsx
@@ -1,9 +1,9 @@
 import { SafeAreaView, View } from 'react-native';
 import React from 'react';
 import { ScreenTitleHeader } from '../../../../Atoms/ScreenTitleHeader';
-import { AppButton } from '../../../../Atoms/AppButton';
 import { MagnifierButton } from '../../../../Molecules/MagnifierButton';
 import { OwnerWorkersMaterialRootProps } from '../../../../../FrontendSelfTypes/navigation/types';
+import { WorkersList } from '../../../../Organisms/WorkersList';
 
 export function WorkersRoot({
   navigation,
@@ -15,24 +15,13 @@ export function WorkersRoot({
   return (
     <SafeAreaView className="w-full h-full">
       <View className="flex-1 ml-4 mr-4">
-        <ScreenTitleHeader variant="lg">Workers</ScreenTitleHeader>
-        <View className="flex items-center w-full flex-row mt-4">
-          <AppButton
-            action={() => ''}
-            context="History"
-            abs="flex-1 h-8 mr-32"
+        <View className="flex mt-4 items-center flex-row justify-between">
+          <ScreenTitleHeader variant="lg">Workers</ScreenTitleHeader>
+          <MagnifierButton
+            onPress={() => navigation.navigate('searchWorker')}
           />
-          <View className="flex-1 flex-row items-center">
-            <AppButton
-              action={() => ''}
-              context="All"
-              abs="flex-1 h-8 bg-[#279840]"
-            />
-            <MagnifierButton
-              onPress={() => navigation.navigate('searchOrder')}
-            />
-          </View>
         </View>
+        <WorkersList />
       </View>
     </SafeAreaView>
   );
