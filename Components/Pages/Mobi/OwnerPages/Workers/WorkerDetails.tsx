3ee508dd24766a9182c3d31542684cf7f9700fdd
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-24 21:22:30 +0200

    split to components

--- Components/Pages/Mobi/OwnerPages/Workers/WorkerDetails.tsx
+++ Components/Pages/Mobi/OwnerPages/Workers/WorkerDetails.tsx
@@ -1,10 +1,14 @@
-import { Image, SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 import { ScreenTitleHeader } from '../../../../Atoms/ScreenTitleHeader';
 import { OwnerWorkersStackProps } from '../../../../../FrontendSelfTypes/navigation/types';
 import { TitleValueInfoComponent } from '../../../../Atoms/TitleValueInfoComponent';
-import { WorkerPosition } from '../../../../../FrontendSelfTypes/moduleProps/ComponentsProps';
+import {
+  WorkerPosition,
+  WorkerStatus,
+} from '../../../../../FrontendSelfTypes/moduleProps/ComponentsProps';
 import { getFontScaledSize } from '../../../../../helpers/style/fontSize';
 import { SquaredProfilePicture } from '../../../../Molecules/SquaredProfilePicture';
+import { CallAndCreateEmailButtons } from '../../../../Molecules/CallAndCreateEmailButtons';
 
 export function WorkerDetails({
   route,
@@ -26,13 +30,46 @@ export function WorkerDetails({
           worker&apos;s information
         </Text>
         <TitleValueInfoComponent
-          titles={['id', 'name', 'surname', 'position']}
+          titles={[
+            'id',
+            'name',
+            'surname',
+            'position',
+            'Status',
+            'dateOfBirth',
+            'address',
+            'dateOfEmployment',
+            'province',
+            'county',
+          ]}
           keys={[
             worker.id.split('-')[worker.id.split('-').length - 1],
             worker.name,
             worker.surname,
             WorkerPosition[worker.position],
+            <Text
+              key={Math.random()}
+              className={`${
+                worker.status === WorkerStatus.Suspended
+                  ? 'text-[#f00]'
+                  : 'text-[#279840]'
+              }`}
+            >
+              {WorkerStatus[worker.status]}
+            </Text>,
+            worker.dateOfBirth,
+            worker.address,
+            worker.dateOfEmployment,
+            worker.province,
+            worker.county,
           ]}
+        />
+        <CallAndCreateEmailButtons
+          phoneNumber={worker.phone}
+          emailOptions={{
+            body: 'Send from FarmService T.M',
+            recipients: [worker.email],
+          }}
         />
       </View>
     </SafeAreaView>
