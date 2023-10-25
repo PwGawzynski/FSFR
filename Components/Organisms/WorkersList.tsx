32047444043dc0794d7ad60dd6fded2d2bc8d7ec
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-25 11:26:42 +0200

    added filter method

--- Components/Organisms/WorkersList.tsx
+++ Components/Organisms/WorkersList.tsx
@@ -3,16 +3,20 @@ import { useQuery } from 'react-query';
 import { getAllWorkers } from '../../helpers/api/Services/Worker';
 import { WorkerComponent } from './WorkerComponent';
 import { SpaceDivider } from '../Molecules/SpaceDivider';
+import { WorkerListProps } from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
 
-export function WorkersList() {
+export function WorkersList({ filterMethod }: WorkerListProps) {
   const { data: workers } = useQuery('workers', () => getAllWorkers());
+  const filtered =
+    (filterMethod && workers && workers.filter(filterMethod)) || workers;
+  console.log('Worker list Render');
   return (
     <FlatList
       keyExtractor={({ id }) => id}
       ItemSeparatorComponent={SpaceDivider}
       showsVerticalScrollIndicator={false}
       className="mt-8"
-      data={workers}
+      data={filtered}
       renderItem={({ item }) => <WorkerComponent item={item} />}
     />
   );
