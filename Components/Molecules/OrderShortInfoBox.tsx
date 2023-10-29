14c25e08cf4bdd0dea63b5d37ca0487f0669abd7
pwgawzynski <pgawzynski.backup@gmail.com>
2023-10-29 21:16:14 +0100

    added styling

--- Components/Molecules/OrderShortInfoBox.tsx
+++ Components/Molecules/OrderShortInfoBox.tsx
@@ -19,17 +19,17 @@ export function OrderShortInfoBox({
   return (
     <TouchableOpacity
       onPress={() => navigation.navigate('orderDetails', { orderId: taskId })}
-      className={`w-max h-32 flex flex-row rounded-2xl border-dotted ${
+      className={`w-max h-[130] flex flex-row rounded-2xl border-dotted ${
         status === OrderStatus.Done ? 'border-[#848484]' : 'border-black'
       } border-2 mb-8`}
     >
-      <TouchableOpacity
+      <View
         className={`w-1/2 h-8 items-center justify-center ${
           status === OrderStatus.Done ? 'bg-[#848484]' : 'bg-black'
         } absolute top-28 left-1/4 rounded-full`}
       >
         <Text className="text-lg font-bold text-white uppercase">More</Text>
-      </TouchableOpacity>
+      </View>
       <View className="flex-col flex-1 flex">
         <View className="flex-1 items-start justify-center ml-2">
           <Text className="text-center font-bold text-lg uppercase">Type</Text>
@@ -39,7 +39,7 @@ export function OrderShortInfoBox({
             Client
           </Text>
         </View>
-        <View className="flex-1 items-start justify-center ml-2">
+        <View className="flex-1 items-start justify-center ml-2 mb-1">
           <Text className="text-center font-bold text-lg uppercase">
             Performance date
           </Text>
@@ -56,7 +56,7 @@ export function OrderShortInfoBox({
             {client}
           </Text>
         </View>
-        <View className="flex-1 items-end justify-center mr-2">
+        <View className="flex-1 items-end justify-center mr-2 mb-1">
           <Text className="text-center font-bold text-lg uppercase">
             {performanceDate}
           </Text>
