4a241aeb5c6aafd48708a776c1d608dbd6d17487
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-22 15:48:48 +0100

    added qr scanner component

---
+++ Components/Organisms/QRScanner.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { QRScannerProps } from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
+
+export function QRScanner({ scanned, handleBarCodeScanned }: QRScannerProps) {
+  const [hasPermission, setHasPermission] = useState<null | boolean>(null);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View className="w-full h-full">
+      {!scanned && (
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      )}
+    </View>
+  );
+}
