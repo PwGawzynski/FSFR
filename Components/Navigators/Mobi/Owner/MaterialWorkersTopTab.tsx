0c47c35efbfffbfa00eeedc6371d876a253a1e27
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-22 23:20:35 +0100

    moved

--- Components/Navigators/Mobi/Owner/MaterialWorkersTopTab.tsx
+++ Components/Navigators/Mobi/Owner/MaterialWorkersTopTab.tsx
@@ -1,9 +1,9 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { MaterialWorkersRootTopTabParamList } from '../../FrontendSelfTypes/NavigatorsInterfaces/MaterialWorkersRootTopTabParamList';
-import { WorkersRoot } from '../Pages/Mobi/OwnerPages/Workers/WorkersRoot';
-import { MaterialTopTabScreenOptions } from '../../helpers/style/MaterialTopTabScreenOptions';
-import { AddWorker } from '../Pages/Mobi/OwnerPages/Workers/AddWorker';
-import { WorkerSearch } from '../Pages/Mobi/OwnerPages/Workers/WorkerSearch';
+import { MaterialWorkersRootTopTabParamList } from '../../../../FrontendSelfTypes/NavigatorsInterfaces/MaterialWorkersRootTopTabParamList';
+import { WorkersRoot } from '../../../Pages/Mobi/OwnerPages/Workers/WorkersRoot';
+import { MaterialTopTabScreenOptions } from '../../../../helpers/style/MaterialTopTabScreenOptions';
+import { AddWorker } from '../../../Pages/Mobi/OwnerPages/Workers/AddWorker';
+import { WorkerSearch } from '../../../Pages/Mobi/OwnerPages/Workers/WorkerSearch';
 
 const MaterialWorkersTopTabNavigator =
   createMaterialTopTabNavigator<MaterialWorkersRootTopTabParamList>();
