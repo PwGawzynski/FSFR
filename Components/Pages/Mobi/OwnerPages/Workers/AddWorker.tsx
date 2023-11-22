5b0356b79c40d69a85b65db945c3887a3b707bc6
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-22 15:49:39 +0100

    changed add worker logic

--- Components/Pages/Mobi/OwnerPages/Workers/AddWorker.tsx
+++ Components/Pages/Mobi/OwnerPages/Workers/AddWorker.tsx
@@ -1,92 +1,89 @@
-import { SafeAreaView, View, Image, TouchableOpacity } from 'react-native';
+import { SafeAreaView, View, Text } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import Animated, {
-  useSharedValue,
-  withDelay,
-  withTiming,
-} from 'react-native-reanimated';
 import { ActivityIndicator } from 'react-native-paper';
+import { useIsFocused } from '@react-navigation/native';
+import { useMutation } from 'react-query';
 import { ScreenTitleHeader } from '../../../../Atoms/ScreenTitleHeader';
-import { NewWorkerSign } from '../../../../../FrontendSelfTypes/moduleProps/ComponentsProps';
-import { getFontScaledSize } from '../../../../../helpers/style/fontSize';
+import {
+  handleBarCodeScannedData,
+  NewWorkerSign,
+} from '../../../../../FrontendSelfTypes/moduleProps/ComponentsProps';
 import { OwnerWorkersMaterialRootProps } from '../../../../../FrontendSelfTypes/navigation/types';
-import Plus from '../../../../../assets/plus.svg';
-import NewWorkerForm from '../../../../Organisms/NewWorkerForm';
-
-const INIT_PROFILE_SIGN: NewWorkerSign = {
-  name: '',
-  surname: '',
-};
-
-const ANIMATION_DELAY = 500;
-const ANIMATION_DURATION = 1000;
+import { QRScanner } from '../../../../Organisms/QRScanner';
+import { addNewWorker } from '../../../../../helpers/api/Services/Worker';
+import { AppButton } from '../../../../Atoms/AppButton';
+import { AppearingText } from '../../../../Molecules/AppearingText';
 
 export function AddWorker({
-  route,
   navigation,
 }: OwnerWorkersMaterialRootProps<'addWorker', 'workersRoot', 'workers'>) {
-  const opacity = useSharedValue(0);
-  const imgUri = route.params?.imgUri;
+  const [scanned, setScanned] = useState(false);
+  const isFocused = useIsFocused();
 
-  const [profileSign, setProfileSign] =
-    useState<NewWorkerSign>(INIT_PROFILE_SIGN);
+  const {
+    mutate: createNewWorker,
+    data: worker,
+    isLoading: isWorkerMutationLoading,
+  } = useMutation(addNewWorker);
 
-  const [isFormPending, setFormPending] = useState(false);
+  const [profileSign, setProfileSign] = useState<NewWorkerSign | undefined>(
+    undefined,
+  );
 
-  const handleProfileSignAnimation = () => {
-    if (profileSign.name.length && profileSign.surname.length)
-      opacity.value = withDelay(
-        ANIMATION_DELAY,
-        withTiming(1, { duration: ANIMATION_DURATION }),
-      );
-    else opacity.value = 0;
+  const handleWorkerAdded = () =>
+    worker &&
+    navigation.navigate('OperationConfirmed', {
+      redirectScreenName: 'materialWorkersRoot',
+      shownMessage: `Worker ${worker.personalData.name} ${worker.personalData.surname} Has Been Added`,
+    });
+
+  useEffect(() => {
+    if (worker) {
+      setProfileSign({
+        name: worker.personalData.name,
+        surname: worker.personalData.surname,
+      });
+    }
+  }, [worker]);
+
+  const handleQrScanned = ({ data }: handleBarCodeScannedData) => {
+    setScanned(true);
+    createNewWorker({ user: data });
   };
-
-  useEffect(() => handleProfileSignAnimation(), [profileSign]);
-
   return (
     <SafeAreaView className="w-full h-full flex flex-col">
-      <View className="ml-4 mr-4 flex-1 flex flex-col justify-between">
+      <View className="ml-4 mr-4 flex-1 flex flex-col">
         <View className="flex-row items-center justify-between">
           <ScreenTitleHeader variant="lg">Add New Worker</ScreenTitleHeader>
-          {isFormPending && <ActivityIndicator size={32} color="#279840" />}
+          {isWorkerMutationLoading && (
+            <ActivityIndicator size={32} color="#279840" />
+          )}
         </View>
-        <View className="flex flex-col items-center justify-center">
-          <TouchableOpacity
-            onPress={() =>
-              navigation.navigate('takePhoto', {
-                onSuccessRedirectionLink: 'addWorker',
-              })
-            }
-            className="h-48 w-48 mt-6 mb-6 bg-[#848484] rounded-lg flex-col items-center justify-center overflow-hidden"
-          >
-            {imgUri ? (
-              <Image
-                source={{
-                  uri: imgUri,
-                }}
-                className="h-full w-full"
+        <View className="flex flex-1 flex-col items-center justify-center">
+          <View className="h-48 w-48 mt-6 mb-6 bg-black rounded-lg flex-col items-center justify-center overflow-hidden">
+            {isFocused && (
+              <QRScanner
+                scanned={scanned}
+                handleBarCodeScanned={handleQrScanned}
               />
-            ) : (
-              <Plus fill="#fff" width={48} height={48} />
             )}
-          </TouchableOpacity>
-          <Animated.Text
-            adjustsFontSizeToFit
-            numberOfLines={1}
-            className="text-center font-semibold"
-            style={{ fontSize: getFontScaledSize(24), opacity }}
-          >
-            {profileSign.name &&
-              profileSign.surname &&
-              `${profileSign.name} ${profileSign.surname}`}
-          </Animated.Text>
+          </View>
         </View>
-        <NewWorkerForm
-          navigation={navigation}
-          setLoadingIndicator={setFormPending}
-          setProfileSign={setProfileSign}
-          profilePhotoUrl={imgUri}
+        <View className="flex-1 items-center justify-center flex-col">
+          {profileSign && (
+            <AppearingText
+              onAnimationEnd={handleWorkerAdded}
+            >{`Added ${profileSign.name} ${profileSign.surname}`}</AppearingText>
+          )}
+        </View>
+        <Text className="mb-8 text-sm text-center text-[#848484] pl-4 pr-4">
+          * To add a new employee, scan the QR code that is available when the
+          employee first logs ba his account
+        </Text>
+        <AppButton
+          abs="mb-8"
+          action={() => setScanned(false)}
+          context="Retry"
         />
       </View>
     </SafeAreaView>
