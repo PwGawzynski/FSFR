1313611a5de58a0d66dfb4390984131a37d89945
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-24 13:00:34 +0100

    changed types due to match witch BE_TYPES

--- Components/Pages/Mobi/OwnerPages/Orders/AllOrders.tsx
+++ Components/Pages/Mobi/OwnerPages/Orders/AllOrders.tsx
@@ -1,31 +1,29 @@
 import { SafeAreaView } from 'react-native';
 import { useState } from 'react';
-import {
-  ActiveFilterValue,
-  OrderBaseI,
-  OrderStatus,
-} from '../../../../../FrontendSelfTypes/moduleProps/ComponentsProps';
+import { ActiveFilterValue } from '../../../../../FrontendSelfTypes/moduleProps/ComponentsProps';
 import { EmptyListInfo } from '../../../../Molecules/EmptyListInfo';
 import { filterByStatus } from '../../../../../helpers/handlers/ordersFilterHandler';
 import { OrdersList } from '../../../../Organisms/OrdersList';
 import { ScreenTitleHeader } from '../../../../Atoms/ScreenTitleHeader';
 import { mapEnumToSubOptionPairs } from '../../../../../helpers/handlers/mapEnumToSubOptionsPairsHandler';
 import { SubOptionFilterSetter } from '../../../../Molecules/SubOptionFilterSetter';
+import { OrderResponseBase } from '../../../../../FarmServiceTypes/Order/Ressponses';
+import { OrderStatus } from '../../../../../FarmServiceTypes/Order/Enums';
 
 export function AllOrders() {
-  const INIT_FILTER_NAME: ActiveFilterValue<OrderBaseI> = {
+  const INIT_FILTER_NAME: ActiveFilterValue<OrderResponseBase> = {
     main: 'status',
     active: { main: 'status' },
   };
   const filterValues = mapEnumToSubOptionPairs(OrderStatus);
   const [filter, setFilter] =
-    useState<ActiveFilterValue<OrderBaseI>>(INIT_FILTER_NAME);
+    useState<ActiveFilterValue<OrderResponseBase>>(INIT_FILTER_NAME);
   return (
     <SafeAreaView className="flex-col h-full justify-center mr-4 ml-4">
       <ScreenTitleHeader variant="lg" abs="mb-8 mt-4">
         All Orders
       </ScreenTitleHeader>
-      <SubOptionFilterSetter<OrderBaseI>
+      <SubOptionFilterSetter<OrderResponseBase>
         options={{ main: 'status', subOptions: filterValues }}
         onFilterOnOff={filterName => setFilter(filterName)}
         filterOn={filter}
