817e86aaac9a970880d082971e4d96fa1e0e81e2
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-24 14:29:51 +0100

    changed  due to connect with db

--- Components/Pages/Mobi/OwnerPages/Orders/OrdersManageWorkers.tsx
+++ Components/Pages/Mobi/OwnerPages/Orders/OrdersManageWorkers.tsx
@@ -3,17 +3,17 @@ import React, { useEffect, useMemo, useState } from 'react';
 import { useQuery } from 'react-query';
 import { HeaderWithButton } from '../../../../Atoms/HeaderWithButton';
 import { OwnerMobiOrdersTopTabProps } from '../../../../../FrontendSelfTypes/navigation/types';
-import {
-  OrderTask,
-  Worker,
-} from '../../../../../FrontendSelfTypes/moduleProps/ComponentsProps';
 import { WorkerSelector } from '../../../../Organisms/WorkerSelector';
 import { getAllOrdersTasks } from '../../../../../helpers/api/Services/Task';
 import { WorkersTasksList } from '../../../../Organisms/WorkersTasksList';
 import { ScreenTitleHeader } from '../../../../Atoms/ScreenTitleHeader';
+import { WorkerResponseBase } from '../../../../../FarmServiceTypes/Worker/Responses';
+import { TaskResponseBase } from '../../../../../FarmServiceTypes/Task/Restonses';
 
-const getExplicitWorkersEntities = (orderAssignedTasks: Array<OrderTask>) => {
-  const filtered: Array<Worker> = [];
+const getExplicitWorkersEntities = (
+  orderAssignedTasks: Array<TaskResponseBase>,
+) => {
+  const filtered: Array<WorkerResponseBase> = [];
   orderAssignedTasks.forEach(
     orderTask =>
       !filtered.find(worker => worker.id === orderTask.worker.id) &&
@@ -27,15 +27,15 @@ export function OrdersManageWorkers({
   route,
 }: OwnerMobiOrdersTopTabProps<'ordersManageWorkers', 'orders'>) {
   const orderId = route.params?.orderId;
-  const [focusedWorker, setFocusedWorker] = useState<Worker | undefined>(
-    undefined,
-  );
+  const [focusedWorker, setFocusedWorker] = useState<
+    WorkerResponseBase | undefined
+  >(undefined);
   const { data: orderAssignedTasks } = useQuery(
     ['orderAssignedTasks', orderId],
     ({ queryKey }) => getAllOrdersTasks(`${queryKey[1]}`),
   );
   const [selectedWorkerTasks, setSelectedWorkerTasks] = useState<
-    Array<OrderTask> | undefined
+    Array<TaskResponseBase> | undefined
   >(undefined);
   useEffect(() => {
     if (focusedWorker && orderAssignedTasks)
@@ -84,7 +84,7 @@ export function OrdersManageWorkers({
           {OrderAssignedWorkerSelector}
         </View>
         <ScreenTitleHeader variant="sm" abs="mt-6  mb-4">
-          {focusedWorker?.name} Task&apos;s
+          {focusedWorker?.personalData.name} Task&apos;s
         </ScreenTitleHeader>
         <View className="flex-1 w-full">{selectedWorkerTasksList}</View>
       </View>
