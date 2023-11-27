2c4e12d5972ade26a52db3e056466e1283b1060f
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-27 20:35:07 +0100

    rebuild due to api types

--- helpers/handlers/workersFilterHandlers.tsx
+++ helpers/handlers/workersFilterHandlers.tsx
@@ -2,10 +2,12 @@ import {
   ActiveFilterValue,
   Worker,
 } from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
+import { WorkerResponseBase } from '../../FarmServiceTypes/Worker/Responses';
+import { PersonalDataBase } from '../../FarmServiceTypes/UserPersonalData/Responses';
 
 export const defaultWorkerFilter = (
-  worker: Worker,
-  filter: ActiveFilterValue<Worker>,
+  worker: WorkerResponseBase,
+  filter: ActiveFilterValue<WorkerResponseBase | PersonalDataBase>,
   searchValue: string,
 ) => {
   if (!filter.active) return true;
@@ -15,5 +17,7 @@ export const defaultWorkerFilter = (
   ) {
     return worker[filter.active.main] === Number(filter.active.subOption);
   }
-  return worker[filter.active.main].toString().includes(searchValue);
+  return (worker.personalData[filter.active.main] as any)
+    ?.toString()
+    .includes(searchValue);
 };
