9bec0383f715656ca0aa45c07341c362e4ea8a5c
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-28 21:41:25 +0100

    added screen

--- FrontendSelfTypes/NavigatorsInterfaces/Worker/WorkerRootStackParamList.tsx
+++ FrontendSelfTypes/NavigatorsInterfaces/Worker/WorkerRootStackParamList.tsx
@@ -1,6 +1,14 @@
 import { WorkerStackParamList } from './WorkerStackParamList';
+import { WorkersStackParamList } from '../WorkersStackParamList';
+import { MaterialWorkersRootTopTabParamList } from '../MaterialWorkersRootTopTabParamList';
 
 export type WorkerRootStackParamList = {
   assignationCheck: undefined;
   desktop: WorkerStackParamList;
+  OperationConfirmed: {
+    redirectScreenName:
+      | keyof WorkersStackParamList
+      | keyof MaterialWorkersRootTopTabParamList;
+    shownMessage?: string;
+  };
 };
