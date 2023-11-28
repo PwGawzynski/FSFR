5d5f73104bb2f4892c3cfba5cb18f101b77b7a18
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-28 21:41:15 +0100

    added screen

--- Components/Navigators/Mobi/Worker/WorkerDesktopStack.tsx
+++ Components/Navigators/Mobi/Worker/WorkerDesktopStack.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { WorkerStackParamList } from '../../../../FrontendSelfTypes/NavigatorsInterfaces/Worker/WorkerStackParamList';
 import { WorkerDesktopMaterial } from './WorkerDesktopMaterial';
+import { Task } from '../../../Pages/Mobi/WorkerPages/Task';
 
 const Stack = createNativeStackNavigator<WorkerStackParamList>();
 
@@ -10,6 +11,14 @@ export function WorkerDesktopStack() {
       <Stack.Screen
         name="root"
         component={WorkerDesktopMaterial}
+        options={{
+          headerShown: false,
+          gestureEnabled: true,
+        }}
+      />
+      <Stack.Screen
+        name="task"
+        component={Task}
         options={{
           headerShown: false,
           gestureEnabled: true,
