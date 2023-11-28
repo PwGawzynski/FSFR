30e974ceed192112b72f1da8306edaa4e69bd68a
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-28 21:41:36 +0100

    added task

--- FrontendSelfTypes/NavigatorsInterfaces/Worker/WorkerStackParamList.tsx
+++ FrontendSelfTypes/NavigatorsInterfaces/Worker/WorkerStackParamList.tsx
@@ -1,5 +1,7 @@
 import { WorkerDesktopMaterialParamList } from './WorkerDesktopMaterialParamList';
+import { TaskResponseBase } from '../../../FarmServiceTypes/Task/Restonses';
 
 export type WorkerStackParamList = {
   root: WorkerDesktopMaterialParamList;
+  task: { task?: TaskResponseBase | undefined };
 };
