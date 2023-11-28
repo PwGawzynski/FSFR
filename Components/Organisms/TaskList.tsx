2427ccc0795aa6244ecc54e2d9d2b3e10c46081c
pwgawzynski <pgawzynski.backup@gmail.com>
2023-11-28 21:39:41 +0100

    added task list component

---
+++ Components/Organisms/TaskList.tsx
@@ -0,0 +1,58 @@
+import React, { memo, useCallback, useMemo } from 'react';
+import { useQuery } from 'react-query';
+import { FlashList } from '@shopify/flash-list';
+import {
+  TaskListItem,
+  TaskListProps,
+} from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
+import { LoadingAnimation } from '../Atoms/LoadingAnimation';
+import { TaskShortInfoBox } from '../Molecules/TaskShortInfoBox';
+import { getTasks } from '../../helpers/api/Services/Worker';
+
+function TaskList({ ListEmptyComponent }: TaskListProps) {
+  const OrderListItem = useCallback(({ item: task }: TaskListItem) => {
+    return (
+      <TaskShortInfoBox
+        field={task.field}
+        id={task.id}
+        type={task.type}
+        worker={task.worker}
+        openedAt={task.openedAt}
+        closedAt={task.closedAt}
+        isDone={task.isDone}
+      />
+    );
+  }, []);
+
+  const { data, isSuccess } = useQuery('tasks', getTasks, {});
+  console.log(data);
+  const list = useMemo(
+    () => (
+      <FlashList
+        onLoad={info => console.log('OrdersList has been loaded in ', info)}
+        estimatedItemSize={130}
+        ListEmptyComponent={ListEmptyComponent}
+        data={data}
+        keyExtractor={item => item.id}
+        renderItem={OrderListItem}
+        className="flex-1 h-max"
+        showsVerticalScrollIndicator={false}
+      />
+    ),
+    [data],
+  );
+
+  return (data && list) || <LoadingAnimation />;
+}
+
+const Orders = memo(
+  TaskList,
+  (prevProps, nextProps) =>
+    prevProps.reloadIndicator === nextProps.reloadIndicator,
+);
+
+Orders.displayName = 'Orders';
+
+// TODO this is not good way to solve this error but enough for now,
+//  change in future https://stackoverflow.com/questions/60386614/how-to-use-props-with-generics-with-react-memo
+export default Orders as typeof TaskList;
