40bf474b6d1417ca26bef4fbac15e305ebc099df
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-01 11:20:50 +0100

    added custom text

--- Components/Atoms/LoadingAnimation.tsx
+++ Components/Atoms/LoadingAnimation.tsx
@@ -2,12 +2,15 @@ import { ActivityIndicator } from 'react-native-paper';
 import { Text, View } from 'react-native';
 import React from 'react';
 
-export function LoadingAnimation() {
+interface Props {
+  customText?: string;
+}
+export function LoadingAnimation({ customText }: Props) {
   return (
     <View className="flex-1 flex-col items-center justify-center">
       <ActivityIndicator size={60} color="#279840" animating />
       <Text className="text-lg text-center mt-4 text-[#848484]">
-        Fetching data...
+        {customText || 'Fetching data...'}
       </Text>
     </View>
   );
