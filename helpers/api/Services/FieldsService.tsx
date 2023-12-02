6011bb170496f15ba9e572d504b2b4f4698ef585
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-02 20:47:24 +0100

    created api cals services

--- helpers/api/Services/FieldsService.tsx
+++ helpers/api/Services/FieldsService.tsx
@@ -2,6 +2,7 @@ import { FieldI } from '../../../FrontendSelfTypes/moduleProps/ComponentsProps';
 import { Api } from '../Api';
 import { FieldResponseBase } from '../../../FarmServiceTypes/Field/Ressponses';
 import { CreateFieldReqI } from '../../../FarmServiceTypes/Field/Requests';
+import { ResponseObject } from '../../../FarmServiceTypes/Respnse/responseGeneric';
 
 export async function getAllFieldsByOrderId(
   id: string,
@@ -16,13 +17,16 @@ export async function getAllFieldsByOrderId(
 
 export async function getAllFieldsById(
   id: string,
-): Promise<FieldI | undefined> {
-  try {
-    const data = await Api.getAllFieldsById(id);
-    return data as FieldI;
-  } catch (e) {
-    return undefined;
-  }
+): Promise<FieldResponseBase | undefined> {
+  const { data } = await Api.getAllFieldsById(id);
+  return data.payload as FieldResponseBase;
+}
+
+export async function remField(
+  id: string,
+): Promise<ResponseObject | undefined> {
+  const { data } = await Api.remField(id);
+  return data as ResponseObject;
 }
 
 export async function createField(
