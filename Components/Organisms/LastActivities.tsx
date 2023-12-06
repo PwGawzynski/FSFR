e65171200386c34d0f95f7e8aba0ead4f4409748
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-06 10:23:48 +0100

    created sse logic

--- Components/Organisms/LastActivities.tsx
+++ Components/Organisms/LastActivities.tsx
@@ -1,65 +1,71 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { useQuery } from 'react-query';
 import { FlashList } from '@shopify/flash-list';
-import {
-  ActivityI,
-  LasActivitiesItem,
-} from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
-import { getLastActivitiesService } from '../../helpers/api/Services/Activities';
+import { MessageEvent } from 'react-native-event-source';
+import { useIsFocused } from '@react-navigation/native';
+import { LasActivitiesItem } from '../../FrontendSelfTypes/moduleProps/ComponentsProps';
 import { Activity } from '../Molecules/Activity';
 import { EmptyListInfo } from '../Molecules/EmptyListInfo';
-import { LoadingAnimation } from '../Atoms/LoadingAnimation';
+import { Api } from '../../helpers/api/Api';
+import { NotificationsResponseBase } from '../../FarmServiceTypes/Notification/Responses';
 
 export function LastActivities() {
-  const [data, setData] = useState<Array<ActivityI> | undefined>(undefined);
-  const { data: activities, isSuccess } = useQuery(
-    'ownerAllActivities',
-    getLastActivitiesService,
-  );
+  const [notifications, setNotifications] = useState<
+    NotificationsResponseBase[] | undefined
+  >(undefined);
+  const [sseOpen, setSseOpen] = useState<boolean>(false);
+  const focused = useIsFocused();
+  const handleOpenSse = () => {
+    setSseOpen(true);
+  };
+  const handleErrorSse = (event: MessageEvent) => console.log(event, 'testER');
+  const handleMessageSse = (message: MessageEvent) => {
+    if (message.data) {
+      const res = JSON.parse(message.data) as NotificationsResponseBase[];
+      setNotifications(res);
+    }
+  };
+
   useEffect(() => {
-    if (activities) setData([...activities]);
-  }, [activities]);
-  useEffect(() => setData(activities), [isSuccess]);
+    return Api.notificationsSse({
+      open: handleOpenSse,
+      error: handleErrorSse,
+      message: handleMessageSse,
+    });
+  }, [!sseOpen, focused]);
 
   const ActivityItem = useCallback(
     ({ item }: LasActivitiesItem) => (
       <Activity
         id={item.id}
-        activityLogCauser={item.activityLogCauser}
-        activityCauserRole={item.activityCauserRole}
-        activityType={item.activityType}
-        taskType={item.taskType}
-        client={item.client}
-        fieldName={item.fieldName}
-        fieldLocationPlaceName={item.fieldLocationPlaceName}
-        date={item.date}
+        shortInfo={item.shortInfo}
+        eventType={item.eventType}
+        createdAt={item.createdAt}
+        causer={item.causer}
+        description={item.description}
       />
     ),
     [],
   );
 
   return useMemo(
-    () =>
-      data ? (
-        <FlashList
-          onLoad={info =>
-            // eslint-disable-next-line no-console
-            console.log(
-              'Owner desktop root lastActivities list has been rendered in: ',
-              info.elapsedTimeInMs,
-            )
-          }
-          keyExtractor={item => item.id}
-          ListEmptyComponent={
-            <EmptyListInfo text={`Empty here :( \n Let's get down to work`} />
-          }
-          renderItem={ActivityItem}
-          data={data}
-          estimatedItemSize={100}
-        />
-      ) : (
-        <LoadingAnimation />
-      ),
-    [data],
+    () => (
+      <FlashList
+        onLoad={info =>
+          // eslint-disable-next-line no-console
+          console.log(
+            'Owner desktop root lastActivities list has been rendered in: ',
+            info.elapsedTimeInMs,
+          )
+        }
+        keyExtractor={item => item.id}
+        ListEmptyComponent={
+          <EmptyListInfo text={`Empty here :( \n Let's get down to work`} />
+        }
+        renderItem={ActivityItem}
+        data={notifications}
+        estimatedItemSize={100}
+      />
+    ),
+    [notifications],
   );
 }
