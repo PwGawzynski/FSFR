5dab2b7eae4b2f859f12c524ab0623f4fdadb193
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-09 20:50:23 +0100

    added services

--- helpers/api/Services/Machine.tsx
+++ helpers/api/Services/Machine.tsx
@@ -1,9 +1,19 @@
 import { Api } from '../Api';
-import { CreateMachineReqI } from '../../../FarmServiceTypes/Machine/Requests';
+import {
+  AssignMachinesReqI,
+  CreateMachineReqI,
+} from '../../../FarmServiceTypes/Machine/Requests';
 import { MachineResponseBase } from '../../../FarmServiceTypes/Machine/Responses';
+import { ResponseObject } from '../../../FarmServiceTypes/Respnse/responseGeneric';
 
 export const createMachine = async (
   data: CreateMachineReqI,
 ): Promise<MachineResponseBase | undefined> => {
   return (await Api.createMachine(data)).data.payload;
+};
+
+export const assignMachine = async (
+  data: AssignMachinesReqI,
+): Promise<ResponseObject | undefined> => {
+  return Api.setMachines(data);
 };
