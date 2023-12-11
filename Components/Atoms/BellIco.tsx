871ba4a2b8e5fafe4b5d9c95f49014f4d8018f0e
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-11 11:40:21 +0100

    test adjustment

--- Components/Atoms/BellIco.tsx
+++ Components/Atoms/BellIco.tsx
@@ -4,7 +4,7 @@ import BellSvg from '../../assets/bell.svg';
 
 export function BellIco() {
   return (
-    <View className="items-end">
+    <View testID="BellIco" className="items-end">
       <BellSvg fill="#000" width={40} height={40} />
       <View className="w-4 h-4 relative top-[-16] right-8 bg-red-600 rounded-full">
         <Text className="text-white text-center">1</Text>
